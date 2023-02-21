aa20ce7ef0990c53120f58f229abc2c5f44cbb4c
Justin Green <justingreendev@gmail.com>
2023-02-20 22:57:52 -0600

    Added balance area

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,20 @@ const Header = () => {
 			<h1>Budget Tracker 2</h1>
 			
 			<div className="c-header__balance-area">
-				<h2 className="c-header__balance-current-title">Balance</h2>
-				<h2 className="c-header__balance-current">$777</h2>
+				<h2 className="c-header__balance-current-label">Balance</h2>
+				<h2 className="c-header__balance-current-title">$1,000.00</h2>
+				
+				<div className="c-header__balance-secondary">
+					<div className="c-header__income-info">
+						<h2 className="c-header__balance-secondary-label">Income</h2>
+						<h2 className="c-header__balance-secondary-title text-green">$2,000.00</h2>
+					</div>
+					
+					<div className="c-header__expense-info">
+						<h2 className="c-header__balance-secondary-label">Expenses</h2>
+						<h2 className="c-header__balance-secondary-title text-red">$1,000.00</h2>				
+					</div>
+				</div>
 			</div>
 		</div>
 	)
